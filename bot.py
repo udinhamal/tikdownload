b71a252ce11a8018720546246c1ca938e6081831
udinhamal <udinhamal0@gmail.com>
2025-09-05 12:11:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -306,4 +306,5 @@ async def main():
         await app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+    asyncio.get_event_loop().run_until_complete(main())
